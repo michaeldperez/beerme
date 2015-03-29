eff302a887ade2732f45569d134cdf3db065055a
Michael Perez <perez.michael.d@gmail.com>
2015-03-29 14:20:09 -0700

    Begin signup routes and index/signup view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,19 @@ get '/' do
   erb :index
 end
 
+post '/users/new' do
+
+  @new_user = User.new(params[:user])
+  if @new_user.save
+    session[:id] = @new_user.id
+    redirect '/users/:id'
+  else
+    @errors = @new_user.errors.full_messages.to_sentence
+    erb :index
+  end
+
+end
+
 post '/' do
  redirect '/'
 end
