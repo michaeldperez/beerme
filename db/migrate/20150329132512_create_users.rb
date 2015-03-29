8855f687f0495fdc13f4606f591f4dcd31e47777
Michael Perez <perez.michael.d@gmail.com>
2015-03-29 13:39:51 -0700

    Create user and favorite models, fill out migrations

--- db/migrate/20150329132512_create_users.rb
+++ db/migrate/20150329132512_create_users.rb
@@ -1,4 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :first_name, :last_name, :email, :password_hash
+      t.timestamps
+    end
   end
 end
--- db/migrate/20150329132543_create_favorites.rb
+++ db/migrate/20150329132543_create_favorites.rb
@@ -1,4 +1,9 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
+    create_table :favorites do |t|
+      t.belongs_to :user
+      t.string :venue, :address, :city, :postal_code, :phone, :url
+      t.integer :latitude, :longitude
+      t.timestamps
   end
 end
