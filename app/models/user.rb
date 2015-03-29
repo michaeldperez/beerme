e78fbccc7a5e61a015fcaf7bfe384c0aed46bf32
Michael Perez <perez.michael.d@gmail.com>
2015-03-29 13:51:43 -0700

    Add validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,29 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  include BCrypt
+
+  has_many :favorites
+
+  validate :first_name, :last_name, :email, :password_hash, { presence: true }
+  validate :email, { uniqueness: true }
+  validates :correct_email_format
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+# setter method:
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def authenticate(input_password)
+    self.password == input_password
+  end
+
+  def correct_email_format
+    self.errors.add :base, "Incorrect email address format" unless /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.match(self.email)
+  end
+
+
 end
