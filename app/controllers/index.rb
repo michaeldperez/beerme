757ec9b31f294e6e486726cc659179649efa8083
Michael Perez <perez.michael.d@gmail.com>
2015-03-29 17:17:21 -0700

    Add background image, add some styling to index page, complete create account and login routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,14 +1,36 @@
 get '/' do
+  @user = User.new
   erb :index
 end
 
+get '/profile' do
+  @user = User.find(session[:id])
+  erb :profile
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
 post '/users' do
-  new_user = User.new(params[:user])
-  if new_user.save
-    session[:id] = new_user.id
-    redirect '/'
+  @user = User.new(params[:user])
+  if @user.save
+    session[:id] = @user.id
+    redirect '/profile'
   else
-    @errors = new_user.errors.full_messages.to_sentence
+    @errors = @user.errors.full_messages.to_sentence
+    erb :index
+  end
+end
+
+post '/login' do
+  @user = User.where(email: params[:email]).first
+  if @user && @user.authenticate(params[:password])
+    session[:id] = @user.id
+    redirect "/profile"
+  else
+    @errors = "Could not find account or/ email did not match account"
     erb :index
   end
 end
