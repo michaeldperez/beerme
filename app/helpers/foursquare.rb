215b73b751aee15104f223392250d987b2a313bb
Michael Perez <perez.michael.d@gmail.com>
2015-03-31 13:50:06 -0700

    Find correct data to present

--- app/helpers/foursquare.rb
+++ app/helpers/foursquare.rb
@@ -15,6 +15,10 @@ helpers do
     JSON.parse(json_obj)
   end
 
+  def to_miles(meters)
+    (meters*0.000621371192).round(2)
+  end
+
   def latlon
     "#{session[:lat]},#{session[:lon]}"
   end
@@ -24,4 +28,13 @@ end
 # HTTParty.get("https://api.foursquare.com/v2/venues/explore?ll=37.7843667,-122.39709250000001&query=beer&section=drinks&client_id=XFMFMCH3CTS0SLTMBQ2KDTFHVJFNPWEPIQU151PPJF5RUZAF&client_secret=NIIUDV3OY5UE5TGZBODQRPS351IVGW5532CV5E4QCQ1VU4CH&v=20150403")
 
 # info = parse(get_fs_data)
-# p info["response"]["groups"][0]["items"][x] where x is venue
+# p info["response"]["groups"][0]["items"][x] where x is hash that includes venue
+
+# info = parse(get_fs_data)
+# test_data = info["response"]["groups"][0]["items"][0]["venue"]
+# p "name: #{test_data["name"]}"
+# p "phone: #{test_data["contact"]["formattedPhone"]}"
+# p "location: #{test_data["location"]["formattedAddress"].join(", ")}"
+# p "distance: #{test_data["location"]["distance"]*0.000621371192} miles"
+# p "people here now: #{test_data["hereNow"]["summary"]}"
+# p "URL: #{test_data["url"]}"
