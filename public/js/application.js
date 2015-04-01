6c018babf5fcbe71fb37454b762755318522f4ab
Michael Perez <perez.michael.d@gmail.com>
2015-03-31 19:28:46 -0700

    Work on favorites AJAX

--- public/js/application.js
+++ public/js/application.js
@@ -1,27 +1,21 @@
-var getTodaysDate = function () {
 
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1;
-  var yyyy = today.getFullYear();
-
-  if(dd<10) {
-      dd = '0' + dd
-  }
-
-  if(mm < 10) {
-      mm = '0' + mm
-  }
-
-  today = yyyy + mm + dd;
-
-  return today;
-}
 
 $(document).ready(function() {
 
-$('.date').html("Today's Date: " + getTodaysDate());
-
-});
+  $('.favorite').on('click', function(event){
+    event.preventDefault();
+    var favorite = $(this).parents().find('.name').val();
+    debugger
+    $.ajax({
+      url: '/profile',
+      type: 'GET',
+      dataType: 'json',
+      // data: {param1: 'value1'},
+    })
+    .done(function() {
+      console.log("success");
+    })
+  });
+})
 
 
