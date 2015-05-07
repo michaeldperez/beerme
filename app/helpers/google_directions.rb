d4eac744d16efa9f7648d38893ac013d6b1edcf0
Michael Perez <perez.michael.d@gmail.com>
2015-05-07 15:43:08 -0700

    Fix get_directions method

--- app/helpers/google_directions.rb
+++ app/helpers/google_directions.rb
@@ -1,16 +1,20 @@
 helpers do
 
   def get_directions(destination)
-    response = HTTParty.get("http://maps.googleapis.com/maps/api/directions/json", query:{
-        origin: latlng,
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json", query:{
+        origin: origin,
         destination: destination,
         key: ENV['direc_key']
       })
     response.body
   end
 
-  def latlng
-    "#{session[:lat]}#{session[:lon]}"
+  def origin
+    "#{session[:lat]},#{session[:lon]}"
+  end
+
+  def destination(coordinates)
+    "#{coordinates[:dlat]},#{coordinates[:dlon]}"
   end
 
 end
