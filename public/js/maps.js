a914e0e11eb2f9550a304e1678a26685534c14ba
Michael Perez <perez.michael.d@gmail.com>
2015-05-06 19:59:15 -0700

    Begin to write jquery for displaying map

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,9 +1,16 @@
+function initialize(lat, lon) {
+  var mapOptions = {
+    center: { lat: lat, lng: lon },
+    zoom: 8
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions); // Here's were modal goes.
+}
+
 $(document).ready(function(){
   $('.map').on('click', function(event){
     event.preventDefault();
-
-    $.ajax({
-      url: '/'
-    })
-  })
+    var lat = ,
+        lon = ;
+    initialize(lat, lon);
 })
