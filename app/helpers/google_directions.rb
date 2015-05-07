5eb124da3e8f073e19409d04b9fb538de80f3b0d
Michael Perez <perez.michael.d@gmail.com>
2015-05-07 14:28:24 -0700

    Create google_directions.rb helper file

--- app/helpers/google_directions.rb
+++ app/helpers/google_directions.rb
@@ -1,3 +1,12 @@
 helpers do
 
+  def get_directions(origin, destination)
+    response = HTTParty.get("http://maps.googleapis.com/maps/api/directions/json", query:{
+        origin:,
+        destination:,
+        key: ENV['direc_key']
+      })
+    response.body
+  end
+
 end
