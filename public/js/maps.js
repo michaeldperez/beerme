e6efd03ed2e906103d5f1cc56ae4d93bc8b2c655
Michael Perez <perez.michael.d@gmail.com>
2015-05-06 20:49:00 -0700

    Add map and marker functionality

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,17 +1,27 @@
-function initialize(lat, lon) {
+function initialize(name, lat, lon) {
+  var location = new google.maps.LatLng(lat, lon);
   var mapOptions = {
-    center: { lat: lat, lng: lon },
-    zoom: 8
+    center: location,
+    zoom: 18
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions); // Here's were modal will go.
+
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: name
+  });
 }
 
 $(document).ready(function(){
   $('.map').on('click', function(event){
     event.preventDefault();
     var venue = $(event.toElement).closest('li'),
-        lat = venue.find('.latitude').html(),
-        lon = venue.find('.longitude').html();
-    initialize(lat, lon);
+        name = venue.find('.name').html(),
+        lat = parseFloat(venue.find('.latitude').html()),
+        lon = parseFloat(venue.find('.longitude').html());
+
+    initialize(name, lat, lon);
+  });
 })
