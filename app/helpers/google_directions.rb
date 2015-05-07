e0fb253f8980a2a011a0331e96390385ad996eb3
Michael Perez <perez.michael.d@gmail.com>
2015-05-07 14:51:25 -0700

    Add latlang method to google_directions.rb helper file

--- app/helpers/google_directions.rb
+++ app/helpers/google_directions.rb
@@ -1,12 +1,16 @@
 helpers do
 
-  def get_directions(origin, destination)
+  def get_directions(destination)
     response = HTTParty.get("http://maps.googleapis.com/maps/api/directions/json", query:{
-        origin:,
-        destination:,
+        origin: latlng,
+        destination: destination,
         key: ENV['direc_key']
       })
     response.body
+  end
+
+  def latlng
+    "#{session[:lat]}#{session[:lon]}"
   end
 
 end
