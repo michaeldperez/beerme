d48054c5dd9c0df4e080b3da9ae5105da040d0a9
Michael Perez <perez.michael.d@gmail.com>
2015-05-18 22:28:46 -0700

    Have profile post route return name of venue as well as redirect uri

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -50,7 +50,7 @@ post '/profile' do
   favorite.url = params[:url]
   if favorite.save
     @user.favorites << favorite
-    { redirect: "/profile" }.to_json
+    { venue: favorite.venue, redirect: "/profile" }.to_json
   else
     status 400
     { errors: "Venue could not be saved." }.to_json
